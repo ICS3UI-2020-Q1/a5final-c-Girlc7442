b495163d7f27cdca6a15ea36bdfb7b6cab557a00
Girlc7442 <girlc7442@wrdsb.ca>
2020-10-07 12:53:25 +0000

    Finished with comments and explained in person

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Counts uo by twos and always starts at an even number
+ * @author Cole Girling
  */
 public class Main {
 
@@ -9,7 +11,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask the user for a starting number
+    System.out.println("What number do you want to begin counting from?");
+    int userStartNumber = input.nextInt();
+
+    //ask the user for an end number
+    System.out.println("What number do you want to end counting at?");
+    int userEndNumber = input.nextInt();
+
+    //if the starting number is odd this will add 1 to it
+    if(userStartNumber % 2 == 1){
+      userStartNumber++;
+    }
+
+    //loop will run while i is equal to or less than the userEndNumber
+    for(int i = userStartNumber; i <= userEndNumber; i += 2){
+      //prints i to the screen
+      System.out.println(i);
+    }
     
   }
 }
